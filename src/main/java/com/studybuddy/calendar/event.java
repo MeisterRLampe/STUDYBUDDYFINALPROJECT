ee3440d8bc3282dc9efa70e38514bc7bf4bd42d5
MarcRoggee <Marc.Roggee@brightstraining@.com>
2023-03-23 11:36:12 +0100

    callender backend beginning

---
+++ src/main/java/com/studybuddy/calendar/event.java
@@ -0,0 +1,22 @@
+package com.studybuddy.calendar;
+
+import com.studybuddy.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class event {
+    @Id
+    private Long id;
+    private String title;
+    private String message;
+    private String timestamp;
+    @ManyToOne
+    private User user;
+
+}
